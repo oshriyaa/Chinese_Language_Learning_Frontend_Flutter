17a0617eced84e1906e504baa86948795c8a85c7
Oshriya Manandhar <manandharoshriya@gmail.com>
2021-11-16 07:14:31 +0545

    initial commit

---
+++ android/app/src/main/kotlin/com/example/chinese_learning/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chinese_learning
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/colors/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static const WHITE = Color(0xFFFFFFFF);
+  // static const CREAM = Color(0xFFF4F0E8);
+  // static const BROWN = Color(0xFFB79980);
+  // static const RED = Color(0xFF833631);
+  // static const BLACK = Color(0XFF000000);
+}
